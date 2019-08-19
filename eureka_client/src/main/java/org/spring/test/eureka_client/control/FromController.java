da95fbef49056b5718a59de2a870337fea32eaa0
TanYang <1240288959@qq.com>
2019-08-19 15:56:05 +0800

    更新

--- eureka_client/src/main/java/org/spring/test/eureka_client/control/FromController.java
+++ eureka_client/src/main/java/org/spring/test/eureka_client/control/FromController.java
@@ -1,6 +1,7 @@
 package org.spring.test.eureka_client.control;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: tanyang
  * @Date: 2019/8/15 12:17
  */
+@RefreshScope
 @RestController
 public class FromController {
 

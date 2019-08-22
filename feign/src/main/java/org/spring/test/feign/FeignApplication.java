313773df5db8e83a87eded126bfbb8f94320f80d
TanYang <1240288959@qq.com>
2019-08-22 15:39:55 +0800

    更新

--- feign/src/main/java/org/spring/test/feign/FeignApplication.java
+++ feign/src/main/java/org/spring/test/feign/FeignApplication.java
@@ -2,13 +2,28 @@ package org.spring.test.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class FeignApplication {
+
+    /*@Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(FeignApplication.class, args);

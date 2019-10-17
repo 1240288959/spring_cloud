716143be515514dd7732bdb6bd97e6ac2d5a5d25
TanYang <1240288959@qq.com>
2019-10-17 17:38:58 +0800

    添加分布式服务跟踪

--- feign/src/main/java/org/spring/test/feign/web/ConsumerController.java
+++ feign/src/main/java/org/spring/test/feign/web/ConsumerController.java
@@ -1,5 +1,6 @@
 package org.spring.test.feign.web;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +15,11 @@ public class ConsumerController {
     @Resource
     private ComputeClient computeClient;
 
+    private final Logger logger = Logger.getLogger(getClass());
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        logger.info("feign 请求计算接口");
         return computeClient.add(a, b);
     }
 }
--- ribbon/src/main/java/org/spring/test/ribbon/service/ComputeService.java
+++ ribbon/src/main/java/org/spring/test/ribbon/service/ComputeService.java
@@ -2,6 +2,7 @@ package org.spring.test.ribbon.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,11 @@ public class ComputeService {
     @Autowired
     private RestTemplate restTemplate;
 
+    private Logger logger = Logger.getLogger(ComputeService.class);
+
     @HystrixCommand(fallbackMethod = "addFallback")
     public String add() {
+        logger.info("请求 http://compute-service/add?a=10&b=20");
         return restTemplate.getForEntity("http://compute-service/add?a=10&b=20", String.class).getBody();
     }
 
